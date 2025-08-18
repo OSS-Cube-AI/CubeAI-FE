5012851e07a5ae0d52dcef75518041f740acb15c
dongmin0204 <nm2205@dgu.ac.kr>
2025-08-18 20:55:15 +0900

    feat: 블록 css 적용

--- src/hooks/dragDrop/NodePalette.tsx
+++ src/hooks/dragDrop/NodePalette.tsx
@@ -1,18 +1,29 @@
 import PaletteItem from "./PaletteItem";
+import Green from '@/assets/block/green.svg';
+import Yellow from '@/assets/block/yellow.svg';
+import Purple from '@/assets/block/purple.svg';
+import Pink from '@/assets/block/pink.svg';
 
 const NODE_DEFS = [
-    { label: "데이터 선택하기", type: "choice" },
-    { label: "빈 데이터 삭제하기", type: "delete" },
-    { label: "손실함수 선택하기", type: "loss function" },
-    { label: "성능 평가하기", type: "performance" },
+    { label: "데이터 선택하기", type: "choice", color: Green, isToggle: false, parameters: [0.003, 10] },
+    { label: "빈 데이터 삭제하기", type: "delete", color: Purple, isToggle: false, parameters: [10] },
+    { label: "손실함수 선택하기", type: "loss function", color: Yellow, isToggle: true, parameters: [] },
+    { label: "성능 평가하기", type: "performance", color: Pink, isToggle: false, parameters: [] },
   ];
   
 export default function NodePalette() {
     return (
-      <div className="w-[336px] h-[74vh] space-y-1 px-4 py-2 text-base">
-        {NODE_DEFS.map((n) => (
+      <>
+      {NODE_DEFS.map((n) => (
+        <div className="relative grid w-[336px] h-28 place-items-center px-4 py-2">
+          <img
+          src={n.color}
+          alt={n.label}
+          className="absolute inset-0 w-full h-full object-contain -z-10 pointer-events-none"
+          />
           <PaletteItem key={n.type} {...n} />
-        ))}
-      </div>
+          </div>
+      ))}
+      </>
     );
   }
