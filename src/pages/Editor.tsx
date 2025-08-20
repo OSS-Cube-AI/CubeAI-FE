72cbd44575cc57ca83e27785bee0ebef86cc22f2
Yunseok <watemark1017@gmail.com>
2025-08-20 18:03:45 +0900

    chore: prettier 한번 적용

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,14 +1,14 @@
-import { useTab } from "@/hooks/useTab";
-import Workspace from "@/components/layout/dragDrop/workspace";
-import NodePalette from "@/hooks/dragDrop/NodePalette";
-import { DragProvider } from "@/hooks/dragDrop/DragContext";
-import DragPreview from "@/hooks/dragDrop/DragPreview";
-import Header from "@/components/layout/Header";
+import { useTab } from '@/hooks/useTab';
+import Workspace from '@/components/layout/dragDrop/workspace';
+import NodePalette from '@/hooks/dragDrop/NodePalette';
+import { DragProvider } from '@/hooks/dragDrop/DragContext';
+import DragPreview from '@/hooks/dragDrop/DragPreview';
+import Header from '@/components/layout/Header';
 
-import AiChatButton from "@/components/editor/AiChatButton";
-import Code from "@/components/editor/rightTab/Code";
-import Data from "@/components/editor/rightTab/Data";
-import Training from "@/components/editor/rightTab/Training";
+import AiChatButton from '@/components/editor/AiChatButton';
+import Code from '@/components/editor/rightTab/Code';
+import Data from '@/components/editor/rightTab/Data';
+import Training from '@/components/editor/rightTab/Training';
 
 export default function EditorPage() {
   const {
@@ -16,7 +16,10 @@ export default function EditorPage() {
     TabTrigger: LeftTabTrigger,
     TabsContainer: LeftTabsContainer,
     TabContent: LeftTabContent,
-  } = useTab<'데이터 전처리' | '모델 설계' | '학습하기' | '평가하기'>('데이터 전처리', 'activeTab-left');
+  } = useTab<'데이터 전처리' | '모델 설계' | '학습하기' | '평가하기'>(
+    '데이터 전처리',
+    'activeTab-left',
+  );
 
   const {
     TabsList: RightTabList,
@@ -35,7 +38,7 @@ export default function EditorPage() {
           <section className="flex justify-between items-end h-[105px] bg-[#EEF6FF] border-b-[2px] border-[#C3CCD9]">
             <div className="w-100 font-bold text-2xl text-center">
               <LeftTabList>
-                <LeftTabTrigger value="데이터 전처리">{"데이터\n전처리"}</LeftTabTrigger>
+                <LeftTabTrigger value="데이터 전처리">{'데이터\n전처리'}</LeftTabTrigger>
                 <LeftTabTrigger value="모델 설계">모델 설계</LeftTabTrigger>
                 <LeftTabTrigger value="학습하기">학습하기</LeftTabTrigger>
                 <LeftTabTrigger value="평가하기">평가하기</LeftTabTrigger>
@@ -51,20 +54,20 @@ export default function EditorPage() {
               </RightTabList>
             </div>
           </section>
-          
+
           <section className="flex justify-between flex-1">
-              {/* 왼쪽 사이드바 */}
-              <aside className="flex w-100 border-r-[2px] border-[#C3CCD9] font-bold text-2xl text-center">
-                <LeftTabsContainer>
-                  <LeftTabContent value="데이터 전처리">
-                    {/* 드래그 가능한 노드 팔레트 */}
-                    <NodePalette />
-                  </LeftTabContent>
-                  <LeftTabContent value="모델 설계">Content B</LeftTabContent>
-                  <LeftTabContent value="학습하기">Content C</LeftTabContent>
-                  <LeftTabContent value="평가하기">Content D</LeftTabContent>
-                </LeftTabsContainer>
-              </aside>
+            {/* 왼쪽 사이드바 */}
+            <aside className="flex w-100 border-r-[2px] border-[#C3CCD9] font-bold text-2xl text-center">
+              <LeftTabsContainer>
+                <LeftTabContent value="데이터 전처리">
+                  {/* 드래그 가능한 노드 팔레트 */}
+                  <NodePalette />
+                </LeftTabContent>
+                <LeftTabContent value="모델 설계">Content B</LeftTabContent>
+                <LeftTabContent value="학습하기">Content C</LeftTabContent>
+                <LeftTabContent value="평가하기">Content D</LeftTabContent>
+              </LeftTabsContainer>
+            </aside>
 
             {/* 여기에 DND 요소 ㄱㄱ */}
             <section className="flex-1 w-full">
@@ -72,12 +75,11 @@ export default function EditorPage() {
               <Workspace />
             </section>
 
-            
             {/* 오른쪽 사이드바 */}
             <aside className="flex w-100 h-full font-bold text-2xl text-center border-l-[2px] border-[#C3CCD9]">
               <RightTabsContainer>
                 <RightTabContent value="코드">
-                  <Code codeString={"import python\n\nprint('Hello, World!')"}/>
+                  <Code codeString={"import python\n\nprint('Hello, World!')"} />
                 </RightTabContent>
                 <RightTabContent value="데이터">
                   <Data />
