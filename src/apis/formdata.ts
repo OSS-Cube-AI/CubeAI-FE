fc150852ecba87bdbd869b86ca0d06c297026b63
dongmin0204 <nm2205@dgu.ac.kr>
2025-08-21 09:28:26 +0900

    hotfix: 환경 변수 세팅

--- src/apis/formdata.ts
+++ src/apis/formdata.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
+import { AI_BACKEND_URL } from '@/constants/api';
 
 export type ServerType = 'MAIN' | 'CHAT' | 'AI';
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 const CHAT_BACKEND_URL = import.meta.env.VITE_CHAT_BACKEND_URL;
-const AI_BACKEND_URL = 'http://211.188.56.255:9022';
 
 const BASE_URLS: Record<ServerType, string> = {
   MAIN: BACKEND_URL,
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,4 @@
 /* API 경로 정의 */
 export const API = {} as const;
 
-export const AI_BACKEND_URL = 'http://211.188.56.255:9022' as const;
+export const AI_BACKEND_URL = import.meta.env.VITE_AI_BACKEND_URL;
